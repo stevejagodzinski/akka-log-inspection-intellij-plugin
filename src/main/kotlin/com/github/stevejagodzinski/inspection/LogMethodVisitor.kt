bba4250f28f686dbc5d61f442d3db46ff0d7d830
Steve Jagodzinski <steve.jagodzinski@gmail.com>
2019-11-01 00:27:17 -0400

    check if method is akka log invocation

--- src/main/kotlin/com/github/stevejagodzinski/inspection/LogMethodVisitor.kt
+++ src/main/kotlin/com/github/stevejagodzinski/inspection/LogMethodVisitor.kt
@@ -1,7 +1,26 @@
 package com.github.stevejagodzinski.inspection
 
 import com.intellij.psi.JavaElementVisitor
+import com.intellij.psi.PsiClassType
+import com.intellij.psi.PsiMethodCallExpression
+import com.intellij.psi.PsiType
 
 class LogMethodVisitor : JavaElementVisitor() {
+    override fun visitMethodCallExpression(expression: PsiMethodCallExpression?) {
+        super.visitMethodCallExpression(expression)
 
+        if (isLogMethod(expression) == true) {
+
+        }
+    }
+
+    private fun isLogMethod(expression: PsiMethodCallExpression?): Boolean? {
+        val returnType = getReturnType(expression)
+        val loggingAdapterType = PsiClassType.getTypeByName("akka.event.LoggingAdapter", expression!!.project, expression.resolveScope)
+        return returnType?.isAssignableFrom(loggingAdapterType);
+    }
+
+    private fun getReturnType(expression: PsiMethodCallExpression?): PsiType? {
+        return expression?.methodExpression?.qualifierExpression?.type
+    }
 }
