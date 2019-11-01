c833562ce6151c10583b206e56034c071a29c4d0
Steve Jagodzinski <steve.jagodzinski@gmail.com>
2019-11-01 00:44:30 -0400

    rm unused imports

--- src/main/kotlin/com/github/stevejagodzinski/inspection/LogMethodVisitor.kt
+++ src/main/kotlin/com/github/stevejagodzinski/inspection/LogMethodVisitor.kt
@@ -2,9 +2,7 @@ package com.github.stevejagodzinski.inspection
 
 import com.intellij.codeInspection.ProblemsHolder
 import com.intellij.psi.JavaElementVisitor
-import com.intellij.psi.PsiClassType
 import com.intellij.psi.PsiMethodCallExpression
-import com.intellij.psi.PsiType
 
 class LogMethodVisitor(holder: ProblemsHolder) : JavaElementVisitor() {
     override fun visitMethodCallExpression(expression: PsiMethodCallExpression?) {
