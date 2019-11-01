71edfdec5303d90ef039fd67c61b1990d14963e4
Steve Jagodzinski <steve.jagodzinski@gmail.com>
2019-11-01 00:55:36 -0400

    rm not needed private method

--- src/main/kotlin/com/github/stevejagodzinski/inspection/LogMethodVisitor.kt
+++ src/main/kotlin/com/github/stevejagodzinski/inspection/LogMethodVisitor.kt
@@ -1,8 +1,10 @@
 package com.github.stevejagodzinski.inspection
 
+import com.github.stevejagodzinski.inspection.IsLogMethodPredicate.isLogMethod
 import com.intellij.codeInspection.ProblemsHolder
 import com.intellij.psi.JavaElementVisitor
 import com.intellij.psi.PsiMethodCallExpression
+
 
 class LogMethodVisitor(private val holder: ProblemsHolder) : JavaElementVisitor() {
     override fun visitMethodCallExpression(expression: PsiMethodCallExpression?) {
@@ -11,10 +13,6 @@ class LogMethodVisitor(private val holder: ProblemsHolder) : JavaElementVisitor(
         if (isLogMethod(expression) == true) {
             checkMethodArguments(expression)
         }
-    }
-
-    private fun isLogMethod(expression: PsiMethodCallExpression?): Boolean? {
-        return IsLogMethodPredicate.isLogMethod(expression)
     }
 
     private fun checkMethodArguments(expression: PsiMethodCallExpression?) {
