810b0862acf1bf382bb6e66b70fd64e9ea02a2ec
Steve Jagodzinski <steve.jagodzinski@gmail.com>
2019-11-06 00:00:00 -0500

    update exception test actor

--- src/test/java/com/github/stevejagodzinski/aliip/actor/ExceptionActor.java
+++ src/test/java/com/github/stevejagodzinski/aliip/actor/ExceptionActor.java
@@ -13,10 +13,13 @@ public class ExceptionActor extends AbstractLoggingActor {
     private void exceptionAfterErrorMessage(final Exception e) {log().error("Log exception after message {}", e);}
     private void exceptionAfterErrorMessageWithPlaceholder(final Exception e) {log().error("Log exception after message {} {}", new Object(), e);}
 
-
     // Exception OK, Placeholder NOK
     private void exceptionAfterWarnMessageWithPlaceholder(final Exception e) {log().warning("Log exception after message {}", new Object(), e);}
     private void exceptionAfterWarnMessageWithItsPlaceholder(final Exception e) {log().warning("Log exception after message {} {}", e);}
+
+    // OK, warn, but exception is provided as placeholder
+    private void exceptionAfterWarnMessageWithExceptionAndObjPlaceholder(final Exception e) {log().warning("Log exception after message {} {}", new Object(), e);}
+    private void exceptionAfterWarnMessageWithExceptionPlaceholder(final Exception e) {log().warning("Log exception after message {}", e);}
 
     // Ok
     private void exceptionAfterErrorMessageWithItsPlaceholder(final Exception e) {log().error(e, "Log exception after message");}
