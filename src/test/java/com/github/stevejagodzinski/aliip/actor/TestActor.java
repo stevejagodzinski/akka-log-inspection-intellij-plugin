9c21e220216db5729f50e1b4e6315c6afac21462
Steve Jagodzinski <steve.jagodzinski@gmail.com>
2019-11-05 22:08:38 -0500

    split ok and nok log calls

--- src/test/java/com/github/stevejagodzinski/aliip/actor/TestActor.java
+++ src/test/java/com/github/stevejagodzinski/aliip/actor/TestActor.java
@@ -7,12 +7,14 @@ import akka.japi.pf.ReceiveBuilder;
 public class TestActor extends AbstractLoggingActor {
     @Override
     public Receive createReceive() {return new ReceiveBuilder().build();}
+    // Not OK
     private void doesLogging(final Exception e) {log().error("Log exception after message", e);}
     private void tooManyParameters0() {log().error("Too many, no placeholder", new Object());}
     private void tooManyParameters1() {log().error("Too many, one placeholder {}", new Object(), new Object());}
     private void tooManyParameters2() {log().error("Too {} many, two placeholders {}", new Object(), new Object(), new Object());    }
     private void tooFewParameters() {log().error("Too {} few, two placeholders {}", new Object());}
     private void tooFewParameters1() {log().error("Too few, one placeholder {}");}
+    // OK
     private void okNoParameters() {log().error("ok");}
     private void okOneParameter() {log().error("ok {}", new Object());}
     private void okTwoParameters() {log().error("ok {}, {}", new Object(), new Object());}
