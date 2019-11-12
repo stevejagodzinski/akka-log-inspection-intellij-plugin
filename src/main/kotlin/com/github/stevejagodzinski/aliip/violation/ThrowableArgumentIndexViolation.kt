8d5df70bd0d43805ecc7c841b91e278884947c2a
Steven Jagodzinski <steve.jagodzinski@gmail.com>
2019-11-12 00:14:20 -0500

    Use extension functions

--- src/main/kotlin/com/github/stevejagodzinski/aliip/violation/ThrowableArgumentIndexViolation.kt
+++ src/main/kotlin/com/github/stevejagodzinski/aliip/violation/ThrowableArgumentIndexViolation.kt
@@ -4,7 +4,7 @@ import com.github.stevejagodzinski.aliip.functions.throwableIndex
 import com.intellij.psi.PsiMethodCallExpression
 
 class ThrowableArgumentIndexViolation(logExpression: PsiMethodCallExpression) : Violation(logExpression) {
-    val throwableIndex = throwableIndex(logExpression)
+    val throwableIndex = logExpression.throwableIndex()
 
     override fun description(): String {
         return "Throwable at argument ($throwableIndex) should be the first argument to log().error(Throwable, String, Object...)"
