9ad567da9e52639e8441ec3a59ac55fdf5913d6a
Steven Jagodzinski <steve.jagodzinski@gmail.com>
2019-11-19 22:48:35 -0500

    Add list box. Use jetbrains swing components

--- src/main/kotlin/com/github/stevejagodzinski/aliip/configuration/component/LogClassConfigurationComponent.kt
+++ src/main/kotlin/com/github/stevejagodzinski/aliip/configuration/component/LogClassConfigurationComponent.kt
@@ -1,22 +1,45 @@
 package com.github.stevejagodzinski.aliip.configuration.component
 
+import com.intellij.ui.components.JBLabel
+import com.intellij.ui.components.JBList
+import com.intellij.ui.components.JBTextField
 import java.awt.GridBagConstraints
+import java.awt.GridBagConstraints.BOTH
 import java.awt.GridBagConstraints.HORIZONTAL
 import java.awt.GridBagLayout
-import javax.swing.JLabel
 import javax.swing.JPanel
-import javax.swing.JTextField
 
 class LogClassConfigurationComponent: JPanel(GridBagLayout()) {
     init {
-        val label = JLabel("Additional Akka Log Class:")
+        createLabel()
+        createTextField()
+
+        createListBox()
+    }
+
+    private fun createLabel() {
+        val label = JBLabel("Additional Akka Log Class:")
         val labelGbc = GridBagConstraints()
         add(label, labelGbc)
+    }
 
-        val field = JTextField()
+    private fun createTextField() {
+        val field = JBTextField()
         val fieldGbc = GridBagConstraints()
         fieldGbc.fill = HORIZONTAL
         fieldGbc.weightx = 1.0
         add(field, fieldGbc)
+    }
+
+    private fun createListBox() {
+        val list = JBList<String>()
+        val listGbc = GridBagConstraints()
+        listGbc.gridwidth = 2
+        listGbc.fill = BOTH
+        listGbc.gridx = 0
+        listGbc.gridy = 1
+        listGbc.weightx = 1.0
+        listGbc.weighty = 1.0
+        add(list, listGbc)
     }
 }
