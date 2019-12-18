abbf4b617ea1dff8a79753bb3eb8c4035637971e
Steven Jagodzinski <steve.jagodzinski@gmail.com>
2019-12-18 10:39:27 -0500

    +log examples w/ message + document Additional Akka Log Class configuration

--- src/test/java/com/github/stevejagodzinski/aliip/actor/custom/CustomLoggingActor.java
+++ src/test/java/com/github/stevejagodzinski/aliip/actor/custom/CustomLoggingActor.java
@@ -3,6 +3,8 @@ package com.github.stevejagodzinski.aliip.actor.custom;
 import com.github.stevejagodzinski.aliip.actor.custom.base.LoggingActor;
 
 @SuppressWarnings("unused")
+// Configured with:
+// Additional Akka Log Class: com.github.stevejagodzinski.aliip.actor.custom.base.LoggingActor.MessageDiagnosticLoggingAdapter
 public class CustomLoggingActor extends LoggingActor {
 
   @Override
@@ -17,6 +19,16 @@ public class CustomLoggingActor extends LoggingActor {
   private void tooManyParameters2() {log().error("Too {} many, two placeholders {}", new Object(), new Object(), new Object());    }
   private void tooFewParameters() {log().error("Too {} few, two placeholders {}", new Object());}
   private void tooFewParameters1() {log().error("Too few, one placeholder {}");}
+
+  // Not OK - with message
+  private final Object message = new Object();
+  private void doesLoggingWithMessage(final Exception e) {log(message).error("Log exception after message", e);}
+  private void tooManyParameterWithMessagesWithMessage0() {log(message).error("Too many, no placeholder", new Object());}
+  private void tooManyParametersWithMessage1() {log(message).error("Too many, one placeholder {}", new Object(), new Object());}
+  private void tooManyParametersWithMessage2() {log(message).error("Too {} many, two placeholders {}", new Object(), new Object(), new Object());    }
+  private void tooFewParametersWithMessage() {log(message).error("Too {} few, two placeholders {}", new Object());}
+  private void tooFewParametersWithMessage1() {log(message).error("Too few, one placeholder {}");}
+
   // OK
   private void okNoParameters() {log().error("ok");}
   private void okOneParameter() {log().error("ok {}", new Object());}
